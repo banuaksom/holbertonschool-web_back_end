7358553c14ac11f9f5ff94599946df7459ae5f10
Banu <banu@Sergeys-MBP.attlocal.net>
2020-10-28 13:13:32 -0700

    added

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -4,9 +4,12 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    ''' BasicCashe class '''
     def put(self, key, item):
+        ''' assigns item for the key '''
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        ''' returns the value linked to the key'''
         return self.cache_data.get(key)
