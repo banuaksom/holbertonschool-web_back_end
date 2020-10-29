b76aae80cbdc70ecbff75a6ff435fcc943539126
Banu <banu@Sergeys-MBP.attlocal.net>
2020-10-28 20:49:43 -0700

    added

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' LIFO caching '''
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    ''' LIFOcashe class '''
+    def __init__(self):
+        ''' init constructor'''
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        ''' assigns item to the key '''
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                removed = self.queue.pop()
+                del self.cache_data[removed]
+                print("DISCARD: " + str(removed))
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Returns the value of the key '''
+        return self.cache_data.get(key)
