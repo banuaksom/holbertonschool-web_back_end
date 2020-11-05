3f2f033f3068785f521b449b7b0c037937d7eb65
Banu <banu@Sergeys-MacBook-Pro.local>
2020-11-04 21:09:09 -0800

    added

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if a password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
