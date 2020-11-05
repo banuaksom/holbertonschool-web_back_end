68b3a8693993b421779fec79a6b01b28977fe570
Banu <banu@Sergeys-MacBook-Pro.local>
2020-11-04 20:47:12 -0800

    added comments

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,24 +6,25 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
+        """ Redacting Formatter """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ filters values in log """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
-    def filter_datum(fields: List[str], redaction: str, message: str,
-                     separator: str) -> str:
-        """returns the log message obfuscated """
-        for f in fields:
-            message = re.sub(f'(?<={f}=)[^{separator}]*', redaction, message)
-        return message
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated """
+    for f in fields:
+        message = re.sub(f'(?<={f}=)[^{separator}]*', redaction, message)
+    return message
