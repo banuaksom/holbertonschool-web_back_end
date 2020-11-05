5d21370d7c9800242990e7716ce5ff1512474e73
Banu <banu@Sergeys-MacBook-Pro.local>
2020-11-04 20:39:51 -0800

    added

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,11 +2,28 @@
 """ Regex Task 0 """
 from typing import List
 import re
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ returns the log message """
-    for f in fields:
-        message = re.sub(f'(?<={f}=)[^{separator}]*', redaction, message)
-    return message
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+    def filter_datum(fields: List[str], redaction: str, message: str,
+                     separator: str) -> str:
+        """returns the log message obfuscated """
+        for f in fields:
+            message = re.sub(f'(?<={f}=)[^{separator}]*', redaction, message)
+        return message
