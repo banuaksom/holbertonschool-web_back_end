ac86bab7cf1afeade102574cde44d9681e1cf660
Banu <banu@Sergeys-MacBook-Pro.local>
2020-11-04 21:07:51 -0800

    added

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Encrypt and validate passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ returns a salted, hashed password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
